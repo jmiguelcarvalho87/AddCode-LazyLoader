93c0141ceaa76427ef6eba25fcb2a1f3a45e580e
barboum <jmiguelcarvalho87@gmail.com>
2021-08-30 20:35:57 +0100

    c# 7 touple

--- AddCodeLazyLoader/LazyLoader/Classes/Evaluator.cs
+++ AddCodeLazyLoader/LazyLoader/Classes/Evaluator.cs
@@ -5,11 +5,11 @@ namespace LazyLoader
 {
     public class Evaluator<T> : IEvaluator<T>
     {
-        List<Tuple<Func<T, T[], T>, T[]>> funcToupleList;
+        List<(Func<T, T[], T>, T[])> funcToupleList;
 
         public Evaluator()
         {
-            funcToupleList = new List<Tuple<Func<T, T[], T>, T[]>>();
+            funcToupleList = new List<(Func<T, T[], T>, T[])>();
         }
 
         public void Add(Func<T, T[], T> func, params T[] additionalArgs)
@@ -17,13 +17,13 @@ namespace LazyLoader
             if (func == null)
                 throw new System.ArgumentException("Func cannot be null");
 
-            funcToupleList.Add(new Tuple<Func<T, T[], T>, T[]>(func, additionalArgs));
+            funcToupleList.Add((func, additionalArgs));
         }
 
         public T Evaluate(T seed)
         {
-            foreach (var touple in funcToupleList)
-                seed = touple.Item1(seed, touple.Item2);
+            foreach (var funcTouple in funcToupleList)
+                seed = funcTouple.Item1(seed, funcTouple.Item2);
 
             return seed;
         }
